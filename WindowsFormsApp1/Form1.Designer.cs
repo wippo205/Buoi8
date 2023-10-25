f0d7b8b37ac101d0ed832ad89c5fb1a6249beef1
NguyenHuuDai <h1h1macon@gmail.com>
2023-10-25 15:43:37 +0700

    update

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_D = new System.Windows.Forms.Button();
+            this.button_U = new System.Windows.Forms.Button();
+            this.button_R = new System.Windows.Forms.Button();
+            this.button_C = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_D
+            // 
+            this.button_D.Location = new System.Drawing.Point(255, 12);
+            this.button_D.Name = "button_D";
+            this.button_D.Size = new System.Drawing.Size(75, 23);
+            this.button_D.TabIndex = 7;
+            this.button_D.Text = "D";
+            this.button_D.UseVisualStyleBackColor = true;
+            // 
+            // button_U
+            // 
+            this.button_U.Location = new System.Drawing.Point(174, 12);
+            this.button_U.Name = "button_U";
+            this.button_U.Size = new System.Drawing.Size(75, 23);
+            this.button_U.TabIndex = 6;
+            this.button_U.Text = "U";
+            this.button_U.UseVisualStyleBackColor = true;
+            // 
+            // button_R
+            // 
+            this.button_R.Location = new System.Drawing.Point(93, 12);
+            this.button_R.Name = "button_R";
+            this.button_R.Size = new System.Drawing.Size(75, 23);
+            this.button_R.TabIndex = 5;
+            this.button_R.Text = "R";
+            this.button_R.UseVisualStyleBackColor = true;
+            // 
+            // button_C
+            // 
+            this.button_C.Location = new System.Drawing.Point(12, 12);
+            this.button_C.Name = "button_C";
+            this.button_C.Size = new System.Drawing.Size(75, 23);
+            this.button_C.TabIndex = 4;
+            this.button_C.Text = "C";
+            this.button_C.UseVisualStyleBackColor = true;
+            this.button_C.Click += new System.EventHandler(this.button_C_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(89, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(610, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_D);
+            this.Controls.Add(this.button_U);
+            this.Controls.Add(this.button_R);
+            this.Controls.Add(this.button_C);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_D;
+        private System.Windows.Forms.Button button_U;
+        private System.Windows.Forms.Button button_R;
+        private System.Windows.Forms.Button button_C;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        ketnoi kn = new ketnoi();
+        SqlConnection connsql;
+        public Form1()
+        {
+            InitializeComponent();
+            connsql = kn.connect;
+        }
+
+        private void button_C_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connsql.Open();
+
+                
+                string insertQuery = "INSERT INTO Khoa (TenKhoa) VALUES (@TenKhoa)";
+                SqlCommand command = new SqlCommand(insertQuery, connsql);
+
+                // Truyền giá trị trực tiếp vào các tham số @MaKhoa và @TenKhoa
+                command.Parameters.AddWithValue("@TenKhoa", "Khoa ThucPham");
+
+                command.ExecuteNonQuery();
+                MessageBox.Show("Dữ liệu Khoa đã được thêm thành công.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                connsql.Close();
+            }
+        }
+        public class ketnoi
+        {
+            public SqlConnection connect;
+            public ketnoi()
+            {
+                connect = new SqlConnection("Data Source = LAPTOP-L0J0D79V\\WIPPOO; Initial Catalog = Net; Integrated Security = True");
+            }
+            public ketnoi(string strcn)
+            {
+                connect = new SqlConnection(strcn);
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=LAPTOP-L0J0D79V\\WIPPOO; Initial Catalog=Net; Integrated Security=True"))
+            {
+                connection.Open();
+                string selectQuery = "SELECT MaKhoa, TenKhoa FROM Khoa";
+                SqlDataAdapter adapter = new SqlDataAdapter(selectQuery, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                connection.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                
+                string MaKhoa = row.Cells["Ma Khoa"].Value.ToString();
+                string TenKhoa = row.Cells["Ten Khoa"].Value.ToString();
+
+                MessageBox.Show($"MaKhoa: {MaKhoa}, TenKhoa: {TenKhoa}");
+            }
+        }
+    }
+}
